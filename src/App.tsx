bfec0c4efa131b2eae649432ddd48d1a4352ffe5
frankdulko <79063207+frankdulko@users.noreply.github.com>
2024-10-03 15:08:00 -0400

    rename pages dir

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import HomePage from './pages/HomePage';
-import ProjectPage from './pages/ProjectPage';
+import HomePage from './app-pages/HomePage';
+import ProjectPage from './app-pages/ProjectPage';
 import Banner from './components/Banner/Banner';
 import Footer from './components/Footer';
 import './App.css';
