480840a9d18adeedddf35e780911e77a522aac9d
frankdulko <79063207+frankdulko@users.noreply.github.com>
2024-10-26 18:08:21 -0400

    clean up

--- src/components/Canvas/Canvas.tsx
+++
@@ -1,9 +0,0 @@
-import useCanvas from "../../hooks/useCanvas";
-import styles from "./style.module.scss";
-
-export const Canvas = (props: any) => {
-  const { draw, ...rest } = props;
-  const canvasRef = useCanvas(draw);
-
-  return <canvas className={styles.canvas} ref={canvasRef} {...rest} />;
-};
